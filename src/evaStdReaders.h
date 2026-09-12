37886c57ea801ea7ac932a9a283f14c7961a4e6e
agusevtec <agusevtec@gmail.com>
2026-09-12 14:56:41 +0300

    add ValueReader

--- src/evaStdReaders.h
+++ src/evaStdReaders.h
@@ -5,6 +5,25 @@
 namespace eva
 {
     /**
+     * @brief Simple stateful reader acting as a bridge between raw control
+     *        values and evaf filters.
+     *
+     * getValue()/isValid() are public because evaf filters call them.
+     * setValue() is public so that decorators can push the target value
+     * straight into the filter.
+     */
+    class ValueReader
+    {
+    private:
+        signed short mValue = 0;
+
+    public:
+        void setValue(signed short value) { mValue = value; }
+        signed short getValue() const { return mValue; }
+        bool isValid() const { return true; }
+    };
+
+    /**
      * @brief Reads digital pin value
      * @tparam tPin Arduino pin number
      * @tparam tPinMode Pin mode (INPUT, INPUT_PULLUP, etc.)
